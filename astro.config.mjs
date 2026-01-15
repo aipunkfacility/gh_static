92977899ccdd33dfd64ac6819225dab124747bc8
AI Punk Facility <aipunkfacility@gmail.com>
2026-01-15 18:49:54 +0700

    Fix: migration to Astro SSG, Tailwind 4 integration and menu fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aipunkfacility.github.io',
-    base: '/gh_static/',
-    output: 'static',
-    build: {
-        assets: 'assets'
-    }
+  site: 'https://aipunkfacility.github.io',
+  base: '/gh_static/',
+  output: 'static',
+
+  build: {
+      assets: 'assets'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
