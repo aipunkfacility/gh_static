50ebcb83b842c2d1b152c661692b38cf613ce54c
AI Punk Facility <aipunkfacility@gmail.com>
2026-01-16 17:03:56 +0700

    Config: remove base path for root domain deployment (Beget)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aipunkfacility.github.io',
-  base: '/gh_static/',
+  // site: 'https://aipunkfacility.github.io',
+  // base: '/gh_static/',
   output: 'static',
 
   build: {
